48426258994bb61751750e8af5341b10948fd9e4
Chris Tack <chris_tack@mac.com>
2024-06-15 10:58:54 -0700

    Add list of screens and params. Ensures navigate function works with Typescript

--- declarations.d.ts
+++ declarations.d.ts
@@ -16,3 +16,39 @@ declare module '*.svg' {
  * Adds a type to SVG imports on mobile.
  */
 declare module '@react-native/metro-config';
+
+// This ensures Typescript is happy when using the navigate function
+export type RootStackParamList = {
+	Login:
+	| {
+		email: string;
+		password: string;
+	}
+	| undefined;
+	Drawer: undefined;
+	DonorDonation: undefined;
+	DonationScreen: undefined;
+	MapScreen: undefined;
+	MakeClaim: {donation: Record, id: number};
+	Logout: undefined;
+	ClaimDetailsScreen: {donation: Record};
+	ClientClaimsScreen: undefined;
+	DonationsDetailScreen: undefined;
+	DeleteAccountScreen: undefined;
+	DeletedAccountScreen: undefined;
+	Register: undefined;
+	TermsScreen: undefined;
+	ContactScreen: undefined;
+	ClientHistoryScreen: undefined;
+	QRCodeScannerScreen: undefined;
+	DonorDashboardScreen: undefined;
+	DonorHistoryScreen: undefined;
+	DashboardScreen: undefined;
+	LogoutScreen: undefined;
+};
+
+declare global {
+	namespace ReactNavigation {
+		interface RootParamList extends RootStackParamList {}
+	}
+}
