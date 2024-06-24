6ebf897ae10abe67a1d41cbfffed5f4e0bca0988
Chris Tack <chris_tack@mac.com>
2024-06-24 12:29:55 -0700

    Fix TypeScript issue when using `navigate()`. Add interfaces for `IDonor`, `IDonation`, `NewDonation`, and `IClaim`

--- declarations.d.ts
+++ declarations.d.ts
@@ -19,36 +19,82 @@ declare module '@react-native/metro-config';
 
 // This ensures Typescript is happy when using the navigate function
 export type RootStackParamList = {
-	Login:
-	| {
-		email: string;
-		password: string;
-	}
-	| undefined;
-	Drawer: undefined;
-	DonorDonation: undefined;
-	DonationScreen: undefined;
-	MapScreen: undefined;
-	MakeClaim: {donation: Record, id: number};
-	Logout: undefined;
-	ClaimDetailsScreen: {donation: Record};
+	ClaimDetailsScreen: { donation: IClaim };
 	ClientClaimsScreen: undefined;
-	DonationsDetailScreen: undefined;
+	ClientHistoryScreen: undefined;
+	ContactScreen: { backDestination: string };
+	DashboardScreen: undefined;
 	DeleteAccountScreen: undefined;
 	DeletedAccountScreen: undefined;
+	DonationScreen: undefined;
+	DonationsDetailScreen: { donation: IDonation };
+	DonorDashboardScreen: undefined;
+	DonorDonationScreen: { donation: IDonation };
+	DonorHistoryScreen: undefined;
+	Drawer: undefined;
+	Login: { email: string; password: string } | undefined;
+	LoginSuccessScreen: undefined;
+	Logout: undefined;
+	LogoutScreen: undefined;
+	MakeClaim: { donation: IDonation; id: number };
+	MakeClaimScreen: { donation: IDonation; id: number };
+	MapScreen: undefined;
+	QRCodeScannerScreen: undefined;
 	Register: undefined;
 	TermsScreen: undefined;
-	ContactScreen: undefined;
-	ClientHistoryScreen: undefined;
-	QRCodeScannerScreen: undefined;
-	DonorDashboardScreen: undefined;
-	DonorHistoryScreen: undefined;
-	DashboardScreen: undefined;
-	LogoutScreen: undefined;
 };
 
 declare global {
 	namespace ReactNavigation {
 		interface RootParamList extends RootStackParamList {}
 	}
+}
+
+export interface IDonor {
+	created_at: Date;
+	email: string;
+	id: number;
+	name: string;
+	phone: string;
+	updated_at: Date;
+}
+
+export interface IDonation {
+	category: string;
+	claim: IClaim;
+	created_at: Date;
+	distance: number;
+	donor_id: number;
+	donor: IDonor;
+	duration_minutes: number;
+	food_name: string;
+	id: number;
+	image_url: string;
+	isHistory?: boolean;
+	measurement: string;
+	per_person: number;
+	pickup_instructions: string;
+	pickup_location: string;
+	status: string;
+	total_amount: string;
+	updated_at: Date;
+}
+
+export type NewDonation = Pick<
+	IDonation,
+	'food_name' | 'category' | 'total_amount' | 'pickup_location' | 'pickup_instructions'
+>;
+
+export interface IClaim {
+	canceled: boolean;
+	claimed: boolean;
+	client_id: number;
+	client_name: undefined;
+	completed: boolean;
+	created_at: Date;
+	donation_id: number;
+	qr_code: string;
+	status: string;
+	time_claimed: Date;
+	updated_at: Date;
 }
