fcb900a2b1ed05591f516f98fb39ace8eb7fbcf0
Chris Tack <chris_tack@mac.com>
2024-07-14 22:37:03 -0700

    Fix map issue

--- src/elements/BananaMap/BananaMap.native.tsx
+++ src/elements/BananaMap/BananaMap.native.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import { DonationMarker } from '@elements/DonationMarker';
 import MapView, { Marker } from 'react-native-maps';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
 import styles from './BananaMap.styles';
 import { BananaMapProps } from './BananaMapProps';
 
 const BananaMap = ({ donations, markerSize, clientLocation, mapRegion }: BananaMapProps) => {
-	const { navigate } = useNavigation();
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	const navigation = useNavigation<NavigationProp<any>>();
+
 	return (
 		<MapView initialRegion={mapRegion} style={styles.map}>
-			{(donations as any).map(donation => {
-				const { id } = donation;
+			{donations.map(donation => {
+				const { id, donor } = donation;
+				const { latitude, longitude } = donor;
+
+				if (latitude == null || longitude == null) {
+					console.warn(`Invalid coordinates for donation ID ${id}: `, latitude, longitude);
+					return null;
+				}
+
 				console.log('Rendering donation marker for donation:', donation);
 				return (
 					<DonationMarker
 						key={id}
-						coordinate={{
-							latitude: parseFloat(donation.donor.latitude),
-							longitude: parseFloat(donation.donor.longitude),
-						}}
+						coordinate={{ latitude, longitude }}
 						size={markerSize}
-						onPress={() => navigate('MakeClaimScreen', { donation, id })}
+						onPress={() => navigation.navigate('MakeClaimScreen', { donation, id })}
 					/>
 				);
 			})}
