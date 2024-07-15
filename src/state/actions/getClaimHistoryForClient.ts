c4b644bf86353a7559956375d88c71aed53a3b02
Chris Tack <chris_tack@mac.com>
2024-07-14 22:18:28 -0700

    Fix issue with displaying client claim history

--- src/state/actions/getClaimHistoryForClient.ts
+++ src/state/actions/getClaimHistoryForClient.ts
@@ -1,12 +1,18 @@
 import railsAxios from '@util/railsAxios';
+import { IClaim } from '../../../declarations';
 
-export const getClaimHistoryForClient = async store => {
+export const getClaimHistoryForClient = async (store: {
+	state: { jwt: string; user: { id: number } };
+	setState: (state: Partial<{ claimHistory: IClaim[] }>) => void;
+}): Promise<IClaim[] | false> => {
 	const { jwt, user } = store.state;
 	const endpoint = `/clients/${user.id}/claims_history`;
 	try {
 		const response = await railsAxios(jwt).get(endpoint);
 		const { data } = response;
-		const sortedData = data.sort((a, b) => a.created_at < b.created_at);
+		const sortedData = data.sort(
+			(a: IClaim, b: IClaim) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+		);
 		if (sortedData) {
 			await store.setState({ claimHistory: sortedData });
 			return sortedData;
