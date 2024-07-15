50bf4dedb62cdb87af840aff5254bca33c6e25a0
Chris Tack <chris_tack@mac.com>
2024-07-14 22:40:37 -0700

    Fix ClientHistoryScreen

--- src/screens/ClientHistoryScreen/ClientHistoryScreen.tsx
+++ src/screens/ClientHistoryScreen/ClientHistoryScreen.tsx
@@ -2,26 +2,25 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
 import { Title, NavBar, EmptyStateView } from '@elements';
-
 import Donation from '@library/DonationClientView/Donation';
 import useGlobal from '@state';
-
 import styles from './ClientHistoryScreen.styles';
-import { UseGlobalType } from '@state/index.types';
+import { UseGlobalType, IClaim } from '@state/index.types';
 
 function ClientHistoryScreen() {
 	const isFocused = useIsFocused();
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const [state, actions] = useGlobal() as UseGlobalType;
-	const [claims, setClaims] = useState([]);
+	const [claims, setClaims] = useState<IClaim[]>([]);
 	const [loaded, setLoaded] = useState(false);
 
 	const getClaims = async () => {
 		const { getClaimHistoryForClient } = actions;
 		const data = await getClaimHistoryForClient();
 		if (data) {
-			setClaims(data);
-			setLoaded(true);
+			setClaims(data as IClaim[]);
 		}
+		setLoaded(true);
 	};
 
 	useEffect(() => {
@@ -33,28 +32,22 @@ function ClientHistoryScreen() {
 	return (
 		<View style={styles.outerContainer}>
 			<NavBar showBackButton={true} />
-
 			<View style={styles.contentContainer}>
 				<Title text="Claims" />
-				<View
-					style={{
-						flexDirection: 'row',
-						justifyContent: 'space-between',
-						alignItems: 'center',
-					}}
-				>
+				<View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
 					<View>
 						<Text style={styles.activeHeader}>History</Text>
 					</View>
 				</View>
-				{!loaded && <Text>Loading...</Text>}
-				{claims && claims.length > 0 ? (
+				{!loaded ? (
+					<Text>Loading...</Text>
+				) : claims.length > 0 ? (
 					<ScrollView>
-						{(claims as any)
-							.sort((a, b) => a.created_at > b.created_at)
+						{claims
+							.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime())
 							.map(claim => (
 								<View key={claim.id}>
-									<Donation donation={claim} key={claim.id} isHistory={true} isClaim={true} />
+									<Donation donation={claim} isHistory={true} isClaim={true} />
 								</View>
 							))}
 					</ScrollView>
