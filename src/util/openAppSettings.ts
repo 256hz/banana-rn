e197935da5288850d36eb7094c9acbdf58fc10ad
Chris Tack <chris_tack@mac.com>
2024-07-21 21:03:41 -0700

    Fix Typescript errors

--- src/util/openAppSettings.ts
+++ src/util/openAppSettings.ts
@@ -1,21 +1,22 @@
 import { Platform } from 'react-native';
 import Constants from 'expo-constants';
-// import { startActivityAsync, ACTION_APPLICATION_DETAILS_SETTINGS } from 'expo-intent-launcher';
+import { startActivityAsync } from 'expo-intent-launcher';
 import { openURL } from 'expo-linking';
-import * as Application from 'expo-application'; // Import the expo-application module
 
 export default Platform.select({
 	ios: async () => {
 		await openURL('app-settings:');
 	},
 	android: async () => {
+		const appOwnership = Constants.appOwnership as 'expo' | 'standalone' | 'guest' | null;
 		const packageName =
-			Constants.appOwnership === 'standalone' ? Constants.expoConfig.android.packageName : 'host.exp.exponent';
-		// await startActivityAsync(ACTION_APPLICATION_DETAILS_SETTINGS,
-		// {
-		// data: `package:${packageName}`,
-		// });
-		await Application.openSettingsAsync();
+			appOwnership === 'standalone'
+				? Constants.expoConfig?.android?.package || 'host.exp.exponent'
+				: 'host.exp.exponent';
+
+		await startActivityAsync('android.settings.APPLICATION_DETAILS_SETTINGS', {
+			data: `package:${packageName}`,
+		});
 	},
 	default: () => Promise.resolve(),
 });
