b26d7ad72c1fdf3738a5c3df017369c76126ce2d
Chris Tack <chris_tack@mac.com>
2024-08-04 17:36:13 -0700

    Remove hardcoded `projectId`

--- app.config.js
+++ app.config.js
@@ -13,11 +13,8 @@ export default ({ config }) => {
 			productionBuild: process.env.ENVIRONMENT_MODE === 'production',
 			googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
 			eas: {
-				projectId: 'ce5107de-81bb-40d1-8cd2-dc61daf0c9d3',
+				projectId: isClient ? process.env.CLIENT_APP_ID : process.env.DONOR_APP_ID,
 			},
-			// eas: {
-			// 	projectId: isClient ? process.env.CLIENT_APP_ID : process.env.DONOR_APP_ID,
-			// },
 		},
 	};
 
